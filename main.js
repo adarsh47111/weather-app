e773790148639a5cdbfef740ceaaab3ed1368f18
adarsh47111 <97270712+adarsh47111@users.noreply.github.com>
2023-08-28 19:19:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+
+const apiKey='56ca2ae0163c4b30e88bb3ac67cc1530';
+const search_icon=document.querySelector(".search-icon")
+const search_box=document.querySelector(".search-box")
+const weather_icon=document.querySelector(".weather-icon")
+const temperature=document.querySelector(".temp")
+const city_name=document.querySelector(".city-name")
+const humidity=document.querySelector(".humidity .val")
+const wind=document.querySelector(".wind .val")
+
+async function updateWeather(city)
+{
+    const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    const data=await response.json();
+
+    weather_icon.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
+    temperature.innerHTML=Math.round(data.main.temp)
+    city_name.innerHTML=data.name
+    humidity.innerHTML=data.main.humidity+"%"
+    wind.innerHTML=data.wind.speed+" km/h"
+    console.log(data)
+}
+
+search_icon.addEventListener("click", ()=>{
+    city=search_box.value
+    city=(city==="")? "New Delhi":city
+    updateWeather(city)
+})
+
+console.log(wind)
